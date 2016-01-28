a8fdf22d129b7d8f4a86c3825dd91a8b961aafbe
Karthik <karthik.bect@gmail.com>
2016-01-28 11:48:24 +0530

    Added Scripts to run gitupdate scripts automatically

--- testpy.sh
+++ testpy.sh
@@ -13,43 +13,31 @@ MAKE_FOLDER_PATH = '/home/{0}/Desktop/'.format(user)
 today = date.today()
 today_folder = today.strftime("%d%m%Y")
 
-yesterday = (today - timedelta(1))
-yesterday_folder = yesterday.strftime("%d%m%Y")
-
-day_before_yesterday = (today - timedelta(2))
-day_before_yesterday_folder = day_before_yesterday.strftime("%d%m%Y")
-
-day_before_yesterdays_yesterday = (today - timedelta(3))
-day_before_yesterdays_yesterday_folder = day_before_yesterdays_yesterday.strftime("%d%m%Y")
-
-#variable used for path
+# variable used for path
 day_0 = '/home/{0}/Desktop/{1}'.format(user, today_folder)
-day_1 = '/home/{0}/Desktop/{1}'.format(user, yesterday_folder)
-day_2 = '/home/{0}/Desktop/{1}'.format(user, day_before_yesterday_folder)
-day_3 = '/home/{0}/Desktop/{1}'.format(user, day_before_yesterdays_yesterday_folder)
 
 
-#conditons checking whether the yesterday_folder exsist or not.
+# conditons checking whether the yesterday_folder exsist or not.
 # variable used here
 z = os.path.exists(day_0)
-a = os.path.exists(day_1)
-b = os.path.exists(day_2)
-c = os.path.exists(day_3)
 
 # path to create Today Folder
 path = '{0}{1}'.format(MAKE_FOLDER_PATH, today_folder)
 
 if not z:
     os.mkdir(path)
+    today_message = 'Today Folder Created'
+    subprocess.Popen(['notify-send', today_message])
+    for i in range(1, 7):
+        to_be_backuped = (today - timedelta(i))
+        to_be_backuped_folder = to_be_backuped.strftime("%d%m%Y")
+        to_be_backuped_path = '/home/{0}/Desktop/{1}'.format(user, to_be_backuped_folder)
+        to_be_backuped_path_check = os.path.exists(to_be_backuped_path)
+        if to_be_backuped_path_check:
+            shutil.move(to_be_backuped_path, BACKUP_FOLDER_PATH)
+            backup_message = '{0} folder backuped'.format(to_be_backuped_folder)
+            subprocess.Popen(['notify-send', backup_message])
 
-    if a:
-        shutil.move(day_1, BACKUP_FOLDER_PATH)
-        print "True - yesterday"
-    elif b:
-        shutil.move(day_2,BACKUP_FOLDER_PATH)
-        print "True - day_before_yesterday"
-    elif c:
-        shutil.move(day_3,BACKUP_FOLDER_PATH)
-        print "True - day_before_yesterdays_yesterday"
-    else:
-        print "No Folders to be backuped"
+else:
+    done_message = 'Already Done Backup'
+    subprocess.Popen(['notify-send', done_message])
